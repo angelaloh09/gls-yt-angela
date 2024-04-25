64978834b77f1657f527339c5c51d2f854b4ab4c
Angela Loh <angelaloh09@gmail.com>
2024-04-24 23:57:59 -0700

    nav menu text color

---
+++ build/precache-manifest.9bcc6e1a45bb1e1364c98a5a69ead657.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfcb5c3ad6f00745c813ec003176bb1",
+    "url": "/gls-yt-angela/index.html"
+  },
+  {
+    "revision": "43e7cbda312be10daf35",
+    "url": "/gls-yt-angela/static/css/main.532bd78a.chunk.css"
+  },
+  {
+    "revision": "68805690b12e8d634dc3",
+    "url": "/gls-yt-angela/static/js/2.c69838cc.chunk.js"
+  },
+  {
+    "revision": "22b6b7433aa4c411f8dae3fa4da162af",
+    "url": "/gls-yt-angela/static/js/2.c69838cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e7cbda312be10daf35",
+    "url": "/gls-yt-angela/static/js/main.4e9646c3.chunk.js"
+  },
+  {
+    "revision": "6cc01f11cfc9da06ed23",
+    "url": "/gls-yt-angela/static/js/runtime-main.9747a203.js"
+  },
+  {
+    "revision": "d86f1113262fc4ed577afed140e5b73c",
+    "url": "/gls-yt-angela/static/media/bookshelf.d86f1113.png"
+  }
+]);
