60ed398292b570e5e4742db288b97644d2be8478
Angela Loh <angelaloh09@gmail.com>
2026-01-04 14:09:02 -0800

    Updates

---
+++ precache-manifest.7d0f7211699e26559328ffafb3f2ef23.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160a0264f6de33ca31607405ff425d56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35389a8682574122841c",
+    "url": "/static/css/main.867f185d.chunk.css"
+  },
+  {
+    "revision": "e62ad750fb8da124a1b4",
+    "url": "/static/js/2.7a038a63.chunk.js"
+  },
+  {
+    "revision": "22b6b7433aa4c411f8dae3fa4da162af",
+    "url": "/static/js/2.7a038a63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35389a8682574122841c",
+    "url": "/static/js/main.7a133044.chunk.js"
+  },
+  {
+    "revision": "113885a85f93ea0d218e",
+    "url": "/static/js/runtime-main.7176250e.js"
+  },
+  {
+    "revision": "ade1dd5b07ea1db6f12628f5b5831694",
+    "url": "/static/media/4884468A-E6D8-410B-8F4F-C32A1777C223.ade1dd5b.JPG"
+  },
+  {
+    "revision": "0776d8e4a96a7e9eb95189dd81968c52",
+    "url": "/static/media/4B4F2FE8-3EF9-40C3-B869-F89F1FB81DDA.0776d8e4.jpg"
+  },
+  {
+    "revision": "eb7b59f09b3315ec839734237d379750",
+    "url": "/static/media/DSCF2560.eb7b59f0.jpg"
+  },
+  {
+    "revision": "03b4a62e6a345feb4b86e7257fd84d34",
+    "url": "/static/media/DSCF2843.03b4a62e.jpg"
+  },
+  {
+    "revision": "5ad449ff24f8811787b377a583f3fdea",
+    "url": "/static/media/DSCF3635.5ad449ff.jpg"
+  },
+  {
+    "revision": "8de2ac21ee969cb0c2585aac4e0b5385",
+    "url": "/static/media/DSCF3890.8de2ac21.jpg"
+  },
+  {
+    "revision": "95e110ed70385d79e4ffed8d76d158bf",
+    "url": "/static/media/DSCF4199.95e110ed.jpg"
+  },
+  {
+    "revision": "a173e952baa81690c4b40b11e89a3231",
+    "url": "/static/media/E43CBFC1-692C-430E-897D-A92BC655F5B7.a173e952.JPG"
+  },
+  {
+    "revision": "fd082297a4e498414b911e3282ddd758",
+    "url": "/static/media/EE4E56DC-D417-4D16-8E7B-F598FC816308.fd082297.jpg"
+  },
+  {
+    "revision": "ba433d95c493254870317bb7c3c143c4",
+    "url": "/static/media/IMG_8878.ba433d95.jpg"
+  },
+  {
+    "revision": "d86f1113262fc4ed577afed140e5b73c",
+    "url": "/static/media/bookshelf.d86f1113.png"
+  }
+]);
