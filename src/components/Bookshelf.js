a388e8385a75b2e6d96e35c64848e6f342be3fed
Angela Loh <angelaloh09@gmail.com>
2026-01-04 15:03:27 -0800

    bulk book add to bookshelf

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -13,41 +13,57 @@ const Bookshelf = () => {
                     <br></br>
                 </div>
                 <div className="cards title">
+                    <h2>2026</h2>
+                </div>
+                {[
+                    {title: "Circe", author: "Madeline Miller"},
+                    {title: "Breakneck: China's Quest to Engineer the Future", author: "Dan Wang"},
+                ].map((book, idx) => (
+                    <div key={idx} className={style.GridContainer}>
+                        <span>{book.title}</span>
+                        <span>{book.author}</span>
+                    </div>
+                ))}
+                <div className="cards title">
                     <h2>2025</h2>
                 </div>
+                {[
+                    {title: "The Alchemist", author: "Paulo Coelho"},
+                    {title: "Open", author: "Andre Agassi"},
+                    {title: "Chip War", author: "Chris Miller"},
+                    {title: "Ultra-processed People", author: "Chris van Tulleken"},
+                    {title: "The Art of Running", author: "Andrea Marcolongo"},
+                    {title: "Galapagos", author: "Kurt Vonnegut"},
+                    {title: "Elon Musk", author: "Walter Isaacson"},
+                    {title: "Norwegian Wood", author: "Haruki Murakami" },
+                    {title: "The Book Thief", author: "Markus Zusak"},
+                ].map((book, idx) => (
+                    <div key={idx} className={style.GridContainer}>
+                        <span>{book.title}</span>
+                        <span>{book.author}</span>
+                    </div>
+                ))}
                 <div className="cards title">
                     <h2>2024</h2>
                 </div>
-
-                <div class = {style.GridContainer}>
-                    <span>How to Avoid a Climate Disaster</span>
-                    <span>Bill Gates</span>
-                </div>
-                
-                <div class = {style.GridContainer}>
-                    <span>What You Are Looking For Is in the Library</span>
-                    <span>Michiko Aoyama</span>
-                </div>
-
-                <div class = {style.GridContainer}>
-                    <span>Three Body Problem</span>
-                    <span>Liu Cixin</span>
-                </div>
-
-                <div class = {style.GridContainer}>
-                    <span>The Idea Factory</span>
-                    <span>Jon Gertner</span>
-                </div>
-
-                <div class = {style.GridContainer}>
-                    <span>Lean In</span>
-                    <span>Sheryl Sandberg</span>
-                </div>
-
-                <div class = {style.GridContainer}>
-                    <span>Bad Blood</span>
-                    <span>John Carreyrou</span>
-                </div>
+                {[
+                    { title: "How to Avoid a Climate Disaster", author: "Bill Gates" },
+                    { title: "Tesla: Inventor of the Electrical Age", author: "W. Bernard Carlson"},
+                    { title: "Born a Crime", author: "Trevor Noah"},
+                    { title: "Principles", author: "Ray Dalio"},
+                    { title: "What You Are Looking For Is in the Library", author: "Michiko Aoyama" },
+                    { title: "Three Body Problem", author: "Liu Cixin" },
+                    { title: "Dark Forest", author: "Liu Cixin"},
+                    { title: "Death's End", author: "Liu Cixin"},
+                    { title: "The Idea Factory", author: "Jon Gertner" },
+                    { title: "Lean In", author: "Sheryl Sandberg" },
+                    { title: "Bad Blood", author: "John Carreyrou" }
+                ].map((book, idx) => (
+                    <div key={idx} className={style.GridContainer}>
+                        <span>{book.title}</span>
+                        <span>{book.author}</span>
+                    </div>
+                ))}
 
             </div>
 
